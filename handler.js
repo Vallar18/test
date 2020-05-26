4799761379036b07873d4bc4e07aeb06aa9415af
tkalenko <tkalenko.yura15@gmail.com>
2020-05-26 13:22:08 +0300

    asdf

---
+++ handler.js
@@ -0,0 +1,12 @@
+// var options = {
+//     name: 'Merchant Name',
+//     description: 'Fine tshirt',
+//     modal: {
+//         handleback: true
+//     }
+// };
+console.log('work');
+document.getElementById('paybtn').onclick = function(){
+    window.alert('click');
+};
+
